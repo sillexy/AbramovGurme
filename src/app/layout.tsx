c5a953fab1e373ba643d190888a4454789ad8aa0
sillexy <ebnfgt@gmail.com>
2026-08-11 06:08:01 +0800

    Load redesigned brand matrix styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "./root-width.css";
 import "./about-layout.css";
 import "./offers-background-fix.css";
+import "./brand-matrix.css";
 
 const display = Cormorant_Garamond({ subsets: ["cyrillic", "latin"], weight: ["500", "600"], variable: "--font-display" });
 const body = Manrope({ subsets: ["cyrillic", "latin"], weight: ["400", "500", "600", "700"], variable: "--font-body" });
