b35902ad6df37ca0fd5637d22632cd8f404ddae6
sillexy <ebnfgt@gmail.com>
2026-08-12 03:45:13 +0800

    style: load micromarket image sizing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ import "./header-logo.css";
 import "./contact-dialog-contacts.css";
 import "./footer-contacts.css";
 import "./safety-reference.css";
+import "./micromarket-image.css";
 
 const display = Cormorant_Garamond({ subsets: ["cyrillic", "latin"], weight: ["500", "600"], variable: "--font-display" });
 const body = Manrope({ subsets: ["cyrillic", "latin"], weight: ["400", "500", "600", "700"], variable: "--font-body" });
