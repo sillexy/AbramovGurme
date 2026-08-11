3847eaf57de63c2a2a3b649c64d8191de3b6234d
sillexy <ebnfgt@gmail.com>
2026-08-12 00:39:48 +0800

    style: load contact dialog contact styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import "./brand-matrix-actions.css";
 import "./brand-matrix-scroll.css";
 import "./offer-price-style.css";
 import "./header-logo.css";
+import "./contact-dialog-contacts.css";
 
 const display = Cormorant_Garamond({ subsets: ["cyrillic", "latin"], weight: ["500", "600"], variable: "--font-display" });
 const body = Manrope({ subsets: ["cyrillic", "latin"], weight: ["400", "500", "600", "700"], variable: "--font-body" });
